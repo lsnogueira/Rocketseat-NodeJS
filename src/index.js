1cf6d84f3148eb2f9d93f55f2a6692d775945fb7
Lucas Nogueira <lucas_snogueira@hotmail.com>
2020-04-14 21:19:38 -0300

    add mocked crud

--- src/index.js
+++ src/index.js
@@ -1,10 +1,61 @@
 const express = require('express');
+const { uuid } = require('uuidv4');
 
 const app = express();
 const port = 3333;
 
-app.get('/', (req, res) => {
-  return res.json({ message: 'Hello World' });
+const techs = [];
+
+app.use(express.json());
+
+app.get('/techs', (req, res) => {
+  const { title } = request.query;
+  const results = title
+    ? techs.filter((tech) => tech.title.includes(title))
+    : techs;
+  return res.json(results);
+});
+
+app.post('/techs', (req, res) => {
+  const { title, owner } = req.body;
+
+  const tech = { id: uuid(), title, owner };
+  techs.push(tech);
+
+  return res.json(tech);
+});
+
+app.put('/techs/:id', (req, res) => {
+  const { id } = req.params;
+  const { title, owner } = req.body;
+
+  const techIndex = techs.findIndex((tech) => tech.id === id);
+
+  if (techIndex < 0) {
+    return res.status(400).json({ error: 'Technology not found' });
+  }
+
+  const tech = {
+    id,
+    title,
+    owner,
+  };
+
+  techs[techIndex] = tech;
+  return res.json(tech);
+});
+
+app.delete('/techs/:id', (req, res) => {
+  const { id } = req.params;
+
+  const techIndex = techs.findIndex((tech) => tech.id === id);
+
+  if (techIndex < 0) {
+    return res.status(400).json({ error: 'Technology not found' });
+  }
+
+  techs.splice(techIndex, 1);
+  return res.status(200).send();
 });
 
 app.listen(port, () => {
